27b6540a3ebdfe0066965fb4e6dd932ad2560838
Yuri Uliam de Lima <yuriuliamdelima@hotmail.com>
2019-03-18 12:18:56 -0300

    Simulador Financeiro Atualizzado
    Não sei porque não enviou nada, mas agora está aqui.

--- Site/js/prototype/prototype.js
+++ Site/js/prototype/prototype.js
@@ -6,3 +6,23 @@ Base criada por Archie
 
 // Script Import
 // Base Code
+
+function calcseed() {
+
+  var seed = semente.value;
+  var seedcount = seedc.value;
+
+  var calcseedvalue = seed * seedcount;
+
+  seedtotal.innerHTML = `Voçê vai gastar em média R$${calcseedvalue.toFixed(2)} com a semente desejada`
+}
+
+function calcstuff() {
+
+  var Pwh = pw.value;
+  var hourpm = hours.value;
+
+  var calcstuffvalue = Pwh * hourpm * 30;
+
+  pwtotal.innerHTML = `Voçê gasta cerca de <strong>R$${calcstuffvalue}Wh</strong> por Mês (30 dias)`
+}
