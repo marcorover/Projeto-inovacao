28f79811ce30a9f091f00ea0fb3ab9e84e56dfef
Yuri Uliam de Lima <yuriuliamdelima@hotmail.com>
2019-03-19 15:46:29 -0300

    Dev-build complete
    Criado um modelo para desenvolvimento instável do projeto

--- Site/js/prototype/prototype.js
+++ Site/js/prototype/prototype.js
@@ -6,23 +6,3 @@ Base criada por Archie
 
 // Script Import
 // Base Code
-
-function calcseed() {
-
-  var seed = semente.value;
-  var seedcount = seedc.value;
-
-  var calcseedvalue = seed * seedcount;
-
-  seedtotal.innerHTML = `Você vai gastar em média R$${calcseedvalue.toFixed(2)} com a semente desejada`
-}
-
-function calcstuff() {
-
-  var Pwh = pw.value;
-  var hourpm = hours.value;
-
-  var calcstuffvalue = Pwh * hourpm * 30;
-
-  pwtotal.innerHTML = `Você gasta cerca de <strong>${calcstuffvalue}Wh</strong> por Mês (30 dias)`
-}
